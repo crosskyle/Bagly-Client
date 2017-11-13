35c850695d143705ba2ca506d9f0be6718e8f42f
crosskyle <kylcross@gmail.com>
2017-11-12 16:59:12 -0800

    Added drop colors

--- src/components/pack-show/PackTable.js
+++ src/components/pack-show/PackTable.js
@@ -30,12 +30,14 @@ const columnStyle = {
 const categoryTarget = {
   drop(props) {
     return props
-  }
+  },
 }
 
-function collect(connect) {
+function collect(connect, monitor) {
   return {
     connectDropTarget: connect.dropTarget(),
+    isOverCurrent: monitor.isOver({ shallow: true }),
+    canDrop: monitor.canDrop()
   }
 }
 
@@ -51,47 +53,57 @@ class PacksTable extends Component {
   }
 
   render() {
-    const { category, pack, connectDropTarget } = this.props
+    const { category, pack, connectDropTarget, isOverCurrent, canDrop} = this.props
+
+    let dropColor = ""
+
+    if (canDrop && isOverCurrent) {
+      dropColor = "#b8de8b"
+    }
+    else if (canDrop) {
+      dropColor = "#b7bacf"
+    }
 
     return connectDropTarget(
       <div style={{ paddingLeft: '.5em', paddingRight: '.5em', marginTop: '15px' }} >
         <Paper>
-        <Table
-          wrapperStyle={{ maxHeight: 300 }}
-          fixedHeader={true}
-          onRowHover={(d) =>  {}/* console.log(category.items[d])*/}>
-          <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-          <TableRow >
-            <TableHeaderColumn colSpan="31" style={{fontSize: '14px'}}>
-              {category.title}
-            </TableHeaderColumn>
-            <TableHeaderColumn style={columnStyle} colSpan="4">
-              <IconButton
-                aria-label="Delete"
-                onClick={() => this.props.deleteCategory(pack.id, category.id)} >
-                <DeleteIcon style={{color: "#D3D3D3"}}/>
-              </IconButton>
-            </TableHeaderColumn>
-          </TableRow>
-          <TableRow>
-            <TableHeaderColumn style={{
-              marginLeft: '0em',
-              paddingLeft: '.5em',
-              marginRight: '0em',
-              paddingRight: '.25em',
-              verticalAlign: 'middle',
-              textAlign: 'left'}} colSpan="12">Name</TableHeaderColumn>
-            <TableHeaderColumn style={columnStyle} colSpan="12">Description</TableHeaderColumn>
-            <TableHeaderColumn style={columnStyle} colSpan="3">Oz</TableHeaderColumn>
-            <TableHeaderColumn style={columnStyle} colSpan="2">Qty</TableHeaderColumn>
-            <TableHeaderColumn style={columnStyle} colSpan="4"> </TableHeaderColumn>
-            <TableHeaderColumn style={columnStyle} colSpan="2"> </TableHeaderColumn>
-          </TableRow>
-          </TableHeader>
-          <TableBody showRowHover={true} displayRowCheckbox={false}>
-          {this.renderItems(category)}
-          </TableBody>
-        </Table>
+          <Table
+            style={{ backgroundColor: dropColor }}
+            wrapperStyle={{ maxHeight: 300 }}
+            fixedHeader={true}
+            onRowHover={(d) =>  {}/* console.log(category.items[d])*/}>
+            <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+            <TableRow >
+              <TableHeaderColumn colSpan="31" style={{fontSize: '14px'}}>
+                {category.title}
+              </TableHeaderColumn>
+              <TableHeaderColumn style={columnStyle} colSpan="4">
+                <IconButton
+                  aria-label="Delete"
+                  onClick={() => this.props.deleteCategory(pack.id, category.id)} >
+                  <DeleteIcon style={{color: "#D3D3D3"}}/>
+                </IconButton>
+              </TableHeaderColumn>
+            </TableRow>
+            <TableRow>
+              <TableHeaderColumn style={{
+                marginLeft: '0em',
+                paddingLeft: '.5em',
+                marginRight: '0em',
+                paddingRight: '.25em',
+                verticalAlign: 'middle',
+                textAlign: 'left'}} colSpan="12">Name</TableHeaderColumn>
+              <TableHeaderColumn style={columnStyle} colSpan="12">Description</TableHeaderColumn>
+              <TableHeaderColumn style={columnStyle} colSpan="3">Oz</TableHeaderColumn>
+              <TableHeaderColumn style={columnStyle} colSpan="2">Qty</TableHeaderColumn>
+              <TableHeaderColumn style={columnStyle} colSpan="4"> </TableHeaderColumn>
+              <TableHeaderColumn style={columnStyle} colSpan="2"> </TableHeaderColumn>
+            </TableRow>
+            </TableHeader>
+            <TableBody showRowHover={true} displayRowCheckbox={false}>
+            {this.renderItems(category)}
+            </TableBody>
+          </Table>
         </Paper>
 
         <ItemAddModal pack={pack} category={category}/>
